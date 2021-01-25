d6a605c0becc9dc286dffb48361ec544b8877580
esabate <eudald.sabate@est.fib.upc.edu>
2021-01-25 12:05:31 +0100

    Updated tracker code with MQTT and correct dump methods. Also updated utils.py with enu2geodetic, pixel2GPS and GPS2pixel methods without hardcoded adfGeoTransform values.

--- tracker.py
+++ tracker.py
@@ -3,9 +3,10 @@ from pycompss.api.task import task
 from pycompss.api.api import compss_barrier, compss_wait_on
 from pycompss.api.constraint import constraint
 from utils import pixel2GPS
+from datetime import datetime
 import track
 import deduplicator as dd
-from datetime import datetime
+import paho.mqtt.client as mqtt
 # import threading
 
 NUM_ITERS = 60
@@ -77,13 +78,16 @@ def receive_boxes(pipe_paths, dummy):
     # This flag serves to know if the video has ended
     cam_id = struct.unpack_from("i", message[1:1 + int_size])[0]
     timestamp = struct.unpack_from("Q", message[1 + int_size:1 + int_size + unsigned_long_size])[0]
+    pixels = [] # for logging purposes it is needed
     for offset in range(1 + int_size + unsigned_long_size, len(message),
                         double_size * 2 + int_size + 1 + float_size * 4):
         north, east, frame_number, obj_class = struct.unpack_from('ddIc', message[
                                                                           offset:offset + double_size * 2 + int_size + 1])
         x, y, w, h = struct.unpack_from('ffff', message[offset + double_size * 2 + int_size + 1:])
         boxes.append(track.obj_m(north, east, frame_number, ord(obj_class), int(w), int(h)))
-    return cam_id, timestamp, boxes, dummy
+        pixels.append((x, y))
+    # return cam_id, timestamp, boxes, dummy # TODO: added x, y (pixels) as they are not in list_boxes anymore
+    return cam_id, timestamp, boxes, dummy, pixels # for logging purposes it is needed
 
 
 @task(returns=3, )
@@ -111,21 +115,29 @@ def deduplicate(trackers_list):
     return return_message
 
 
-def dump(id_cam, ts, trackers, iteration):
+def dump(id_cam, ts, trackers, iteration, list_boxes, info_for_deduplicator, pixels):
     import pygeohash as pgh
     import os
-    if not os.path.exists("singlecamera.in"):
-        f = open("singlecamera.in", "w+")
+    filename = "singlecamera.in"
+    if not os.path.exists(filename):
+        f = open(filename, "w+")
         f.close()
-    with open("singlecamera.in", "a+") as f:
-        for tracker in trackers:
-            lat, lon = pixel2GPS(tracker.traj[-1].x, tracker.traj[-1].y)
+    with open(filename, "a+") as f:
+        for i, tracker in enumerate([t for t in trackers if t.traj[-1].frame == iteration]):
+            lat = info_for_deduplicator[i][0]  # round(info_for_deduplicator[i][0], 14)
+            lon = info_for_deduplicator[i][1]  # round(info_for_deduplicator[i][1], 14)
             geohash = pgh.encode(lat, lon, precision=7)
-            speed = abs(tracker.ekf.xEst.vel)
-            speed = speed / 2
-            yaw = tracker.ekf.xEst.yaw
+            cl = info_for_deduplicator[i][2]
+            speed = info_for_deduplicator[i][3]
+            yaw = info_for_deduplicator[i][4]
+            pixel_x = pixels[tracker.idx][0]
+            pixel_y = pixels[tracker.idx][1]
+            # f.write(
+            # f"{id_cam} {iteration} {ts} {cl} {lat:.14f} {lon:.14f} {geohash} {speed} {yaw} {id_cam}_{tracker.id} \
+            # {tracker.idx].x} {list_boxes[tracker.idx].y} {list_boxes[tracker.idx].w} {list_boxes[tracker.idx].h}\n")
             f.write(
-                f"{id_cam} {iteration} {ts} {tracker.cl} {lat} {lon} {geohash} {speed} {yaw} {id_cam}_{tracker.id}\n")
+                f"{id_cam} {iteration} {ts} {cl} {lat} {lon} {geohash} {speed} {yaw} {id_cam}_{tracker.id} {pixel_x} \
+                {pixel_y} {list_boxes[tracker.idx].w} {list_boxes[tracker.idx].h}\n")
 
 
 @constraint(AppSoftware="xavier")
@@ -136,7 +148,7 @@ def persist_info_accumulated(trackers_list, count, kb):
     snapshot = EventsSnapshot(snapshot_alias)
     kb.add_events_snapshot(snapshot) # persists snapshot
     for trackers in trackers_list:
-        snapshot.add_events_from_trackers(trackers, kb)  # create events inside dataclay
+        snapshot.add_events_from_trackers(trackers, kb) # create events inside dataclay
     return snapshot
 
 
@@ -227,13 +239,13 @@ def init_task():
     event.make_persistent("FAKE_EVENT_" + str(uuid.uuid4()))
     obj = Object("FAKE_OBJ_" + str(uuid.uuid4()), "FAKE")
     obj.make_persistent("FAKE_OBJ_" + str(uuid.uuid4()))
-    obj.get_events_history()
+    obj.get_events_history(20)
     list_objs = ListOfObjects()
     list_objs.make_persistent("FAKE_LISTOBJ_" + str(uuid.uuid4()))
     list_objs.get_or_create("FAKE_LISTOBJ_" + str(uuid.uuid4()), "FAKE")
     federation_info = FederationInfo([snap])
     federation_info.make_persistent()
-    federation_info.objects_per_snapshot  # to load dataclay class and libraries
+    federation_info.objects_per_snapshot # to load dataclay class and libraries
 
 
 @constraint(AppSoftware="nvidia")
@@ -257,8 +269,8 @@ def execute_trackers(pipe_paths, kb):
     cam_ids = [0] * len(socket_ips)
     timestamps = [0] * len(socket_ips)
     deduplicated_trackers_list = []  # TODO: accumulate trackers
+    pixels = [0] * len(pipe_paths)
 
-    # video_resolution = (1920, 1080)  # TODO: RESOLUTION SHOULD NOT BE HARDCODED!
     video_resolution = (3072, 1730)  # TODO: RESOLUTION SHOULD NOT BE HARDCODED!
     reference_x, reference_y = [r // 2 for r in video_resolution]
 
@@ -272,13 +284,15 @@ def execute_trackers(pipe_paths, kb):
     start_time = time.time()
     while i < NUM_ITERS:
         for index, pipe_path in enumerate(pipe_paths):
-            cam_ids[index], timestamps[index], list_boxes, reception_dummies[index] = \
+            # cam_ids[index], timestamps[index], list_boxes, reception_dummies[index] = \ # without pixels
+            cam_ids[index], timestamps[index], list_boxes, reception_dummies[index], pixels[index] = \
                 receive_boxes(pipe_path, reception_dummies[index])
             trackers_list[index], cur_index[index], info_for_deduplicator[index] = execute_tracking(list_boxes,
                                                                                                     trackers_list[index],
                                                                                                     cur_index[index])
             # print(f"CAM ID: {cam_ids[index]}, timestamp: {timestamps[index]}, list_boxes: {[list_boxes]}")
-            # dump(cam_ids[index], timestamps[index], trackers_list[index], i)
+            # dump(cam_ids[index], timestamps[index], trackers_list[index], i, list_boxes, \
+            # info_for_deduplicator[index], pixels[index])
 
         # trackers, tracker_indexes, cur_index = merge_tracker_state(trackers_list)
         deduplicated_trackers = deduplicate(info_for_deduplicator) # , cam_ids, timestamps) # TODO: pass cam_ids and timestamps
@@ -293,9 +307,9 @@ def execute_trackers(pipe_paths, kb):
         """
         snapshots.append(snapshot)
         if i != 0 and (i+1) % SNAP_PER_FEDERATION == 0:
-            federate_info_accumulated(snapshots, dataclay_to_federate)
+            federate_info_accumulated(snapshots, kb, dataclay_to_federate)
         """
-        federate_info(snapshot, dataclay_to_federate)
+        federate_info(snapshot, kb, dataclay_to_federate)
         i += 1
         if i != 0 and i % 10 == 0:
             compss_barrier()
@@ -308,20 +322,46 @@ def execute_trackers(pipe_paths, kb):
     print("Exec Inner Time: " + str(end_time - start_time))
     print("Exec Inner Time per Iteration: " + str((end_time - start_time) / NUM_ITERS))
 
+
+def on_message(client, userdata, message):
+    import time
+    received_time = time.time()
+    msg = str(message.payload.decode('utf-8'))
+    print(f"Received message = {msg}\nat time {received_time}")
+
+
+def publish_mqtt():
+    client = mqtt.Client()
+    client.connect("192.168.7.41")
+    client.publish("test", "Start of the execution of the COMPSs workflow")
+
+
+def register_mqtt():
+    client=mqtt.Client()
+    client.connect("192.168.7.41") # MQTT server in Modena cloud
+    client.on_message=on_message
+    client.subscribe("test")
+
+
 def main():
     import time
-    from dataclay.api import init, register_dataclay, finish
+    from dataclay.api import init, finish
     from dataclay.exceptions.exceptions import DataClayException
 
     init()
     from CityNS.classes import DKB, ListOfObjects
-    # register_dataclay("192.168.7.32", 21034)
+
+    # Register MQTT client to subscribe to MQTT server in 192.168.7.41
+    register_mqtt()
 
     # initialize all computing units in all workers
     num_cus = 8
     for i in range(num_cus):
         init_task()
     compss_barrier()
+
+    # Publish to the MQTT broker that the execution has started
+    publish_mqtt()
 
     try:
         kb = DKB.get_by_alias("DKB")
@@ -335,7 +375,6 @@ def main():
     start_time = time.time()
     # execute_trackers(["192.168.50.103"], kb)
     execute_trackers([("/tmp/pipe_yolo2COMPSs", "/tmp/pipe_COMPSs2yolo")], kb)
-
     # print("ExecTime: " + str(time.time() - start_time))
     # print("ExecTime per Iteration: " + str((time.time() - start_time) / NUM_ITERS))
     print("Exiting Application...")
--- utils.py
+++ utils.py
@@ -9,7 +9,27 @@ def category_parse(number):
             5: dd.Categories.C_bycicle}.get(number, None)
 
 
-def pixel2GPS(x, y):
+def enu2GPS(x, y):
     import pymap3d as pm
     lat, lon, _ = pm.enu2geodetic(x, y, 0, 44.655540, 10.934315, 0)
     return lat, lon
+
+def pixel2GPS(tif_file, x, y):
+    from osgeo import gdal
+    ds = gdal.Open(tif_file)
+    adfGeoTransform = ds.GetGeoTransform()
+    xoff = adfGeoTransform[0]
+    a = adfGeoTransform[1]
+    b = adfGeoTransform[2]
+    yoff = adfGeoTransform[3]
+    d = adfGeoTransform[4]
+    e = adfGeoTransform[5]
+    return d * x + e * y + yoff, a * x + b * y + xoff
+
+def GPS2pixel(tif_file, lat, lon):
+    from osgeo import gdal
+    ds = gdal.Open(tif_file)
+    adfGeoTransform = ds.GetGeoTransform()
+    x = int(round((lon - adfGeoTransform[0]) / adfGeoTransform[1]))
+    y = int(round((lat - adfGeoTransform[3]) / adfGeoTransform[5]))
+    return x, y
