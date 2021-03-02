d227378255b1c853201e289102984b668264871d
esabate <eudald.sabate@est.fib.upc.edu>
2021-03-02 10:53:37 +0100

    dataclay_init uploaded

---
+++ dataclay/dataclay_init.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -x
+set -e
+
+CONTRACT_ID_FILE=${DC_SHARED_VOLUME}/${NAMESPACE}_contractid
+
+########################### create cfgfiles ###########################
+
+printf "HOST=${LOGICMODULE_HOST}\nTCPPORT=${LOGICMODULE_PORT_TCP}" > ${DATACLAYCLIENTCONFIG}
+echo "Account=${USER}
+Password=${PASS}
+DataSets=${DATASET}
+DataSetForStore=${DATASET}
+StubsClasspath=${STUBSPATH}" > ${DATACLAYSESSIONCONFIG}
+
+######################################################
+
+# Wait for contract id in shared volume
+while [ ! -f ${CONTRACT_ID_FILE} ]; do echo "Waiting for contract ID at ${CONTRACT_ID_FILE}..."; sleep 5; done
+
+export CONTRACT_ID=`cat ${CONTRACT_ID_FILE}`
+
+mkdir -p ${STUBSPATH}
+# Execute command
+exec python ../tracker.py "$@"
