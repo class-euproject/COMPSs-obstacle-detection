a6be8d397d7ae5671c49431a01ded1126a13e0ac
esabate <eudald.sabate@est.fib.upc.edu>
2021-03-12 13:18:52 +0100

    Entrypoint updated

--- entrypoint.py
+++ entrypoint.py
@@ -15,6 +15,9 @@ def main():
     while contract_id is None:
         try:
             contract_id = subprocess.check_output(f"java -cp {dataclay_jar_path} es.bsc.dataclay.tool.AccessNamespace {user} {password} {namespace} | tail -1", shell=True)[:-1].decode()
+            print(f"CONTRACT ID IS {contract_id}")
+            if contract_id == "":
+                contract_id = None
         except:
             contract_id = None
             print(f"Waiting for contract_id to be set and ready from dataclay...")
