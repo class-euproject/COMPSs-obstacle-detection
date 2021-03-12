130b6f16f22b07308a67bd51e8c1ef9af27f3d37
esabate <eudald.sabate@est.fib.upc.edu>
2021-03-12 13:11:31 +0100

    Updated entrypoint.py to wait for dataclay to finish.

--- entrypoint.py
+++ entrypoint.py
@@ -10,7 +10,13 @@ def main():
     stubspath = "./stubs"
     
     # Connection to get contract_id
-    contract_id = subprocess.check_output(f"java -cp {dataclay_jar_path} es.bsc.dataclay.tool.AccessNamespace {user} {password} {namespace} | tail -1", shell=True)[:-1].decode()
+    contract_id = None
+    while contractd_id is None:
+        try:
+            contract_id = subprocess.check_output(f"java -cp {dataclay_jar_path} es.bsc.dataclay.tool.AccessNamespace {user} {password} {namespace} | tail -1", shell=True)[:-1].decode()
+        except:
+            contract_id = None
+            print(f"Waiting for contract_id to be set and ready from dataclay.")
 
     # Get stubs
     get_stubs(user, password, contract_id, stubspath)
