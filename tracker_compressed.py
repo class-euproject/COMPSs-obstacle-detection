edfd25842961ef1163b3b278663bf95008e5d0f9
eudaldsabate <eudald.sabate@est.fib.upc.edu>
2021-05-11 11:52:41 +0200

    Added str2bool method in tracker_compressed.py

--- tracker_compressed.py
+++ tracker_compressed.py
@@ -414,6 +414,17 @@ def register_mqtt():
     return client
 
 
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
 def main():
     import sys
     import time
